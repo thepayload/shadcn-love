fbfb0594304d91fc79290b05c2d77217f550e6ca
Miguel <85888237+imkhoat@users.noreply.github.com>
2024-06-22 15:45:52 +0700

    feat: add label prop for separator component (#5)

--- apps/www/src/lib/registry/new-york/ui/separator/Separator.vue
+++ apps/www/src/lib/registry/new-york/ui/separator/Separator.vue
@@ -3,7 +3,7 @@ import { type HTMLAttributes, computed } from 'vue'
 import { Separator, type SeparatorProps } from 'radix-vue'
 import { cn } from '@/lib/utils'
 
-const props = defineProps<SeparatorProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<SeparatorProps & { class?: HTMLAttributes['class'], label?: string }>()
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props
@@ -15,6 +15,15 @@ const delegatedProps = computed(() => {
 <template>
   <Separator
     v-bind="delegatedProps"
-    :class="cn('shrink-0 bg-border', props.orientation === 'vertical' ? 'w-px h-full' : 'h-px w-full', props.class)"
-  />
+    :class="cn('shrink-0 bg-border relative', props.orientation === 'vertical' ? 'w-px h-full' : 'h-px w-full', props.class)"
+  >
+    <span
+      v-if="props.label"
+      class="text-xs text-muted-foreground absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white flex justify-center items-center"
+      :class="{
+        'h-[1px]': props.orientation !== 'vertical',
+        'w-[1px]': props.orientation === 'vertical',
+      }"
+    >{{ props.label }}</span>
+  </Separator>
 </template>
