6b5b58b0253672a8337332d578b035abf8cec984
Miguel <85888237+imkhoat@users.noreply.github.com>
2024-06-30 14:17:35 +0700

    feat: implement avatar wrapper component (#9)

---
+++ apps/www/src/lib/registry/default/example/AvatarWrapperDemo.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { AvatarWrapper } from '@/lib/registry/default/ui/avatar'
+</script>
+
+<template>
+  <AvatarWrapper src="https://github.com/radix-vue.png" alt="@radix-vue" fallback="CN" />
+</template>
