8873f72eac57f4c40a235f98b1d5593f32a75903
Miguel <85888237+imkhoat@users.noreply.github.com>
2024-06-30 13:51:50 +0700

    feat: implement wrapper alert component (#8)

--- apps/www/src/lib/registry/default/ui/alert/index.ts
+++ apps/www/src/lib/registry/default/ui/alert/index.ts
@@ -2,6 +2,7 @@ import { type VariantProps, cva } from 'class-variance-authority'
 
 export { default as Alert } from './Alert.vue'
 export { default as AlertTitle } from './AlertTitle.vue'
+export { default as AlertWrapper } from './AlertWrapper.vue'
 export { default as AlertDescription } from './AlertDescription.vue'
 
 export const alertVariants = cva(
