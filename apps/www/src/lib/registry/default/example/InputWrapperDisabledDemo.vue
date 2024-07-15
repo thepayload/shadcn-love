a4b8b90e13495f467fc314c4d2768715051fcf59
Miguel <85888237+imkhoat@users.noreply.github.com>
2024-07-15 21:46:16 +0700

    feat(pro): implement input wrapper component (#22)

---
+++ apps/www/src/lib/registry/default/example/InputWrapperDisabledDemo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { InputWrapper } from '@/lib/registry/default/ui/input'
+
+const email = ref('')
+</script>
+
+<template>
+  <InputWrapper v-model="email" :disabled="true" label="Email" type="email" description="Only allow email with @" placeholder="Type your email here" />
+</template>
