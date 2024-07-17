6b2ffb7ea21b7a309e9794d95b89a00944c9f90b
imkhoat <khoatbui.idea@gmail.com>
2024-07-17 20:26:53 +0700

    feat: implement text wrapper component

--- apps/www/.vitepress/theme/config/docs.ts
+++ apps/www/.vitepress/theme/config/docs.ts
@@ -538,7 +538,6 @@ export const docsConfig: DocsConfig = {
         {
           title: 'Textarea',
           href: '/pro/wrappers/textarea',
-          disabled: true,
         },
         {
           title: 'Toggle',
