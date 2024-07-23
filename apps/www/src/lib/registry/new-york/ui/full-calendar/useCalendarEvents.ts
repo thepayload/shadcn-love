d8a42950a245c1aca211054c7d338ca3b4a49e14
Miguel <85888237+imkhoat@users.noreply.github.com>
2024-07-23 23:03:25 +0700

    feat(pro): implement full calendar component (#26)
    * feat(pro): implement number input wrapper component (#24)
    
    * feat(pro): implement full calendar component

---
+++ apps/www/src/lib/registry/new-york/ui/full-calendar/useCalendarEvents.ts
@@ -0,0 +1,29 @@
+import { computed } from 'vue'
+import type { CalendarEvent } from '.'
+
+export function useCalendarEvents(events: CalendarEvent[]) {
+  const mapEvents = computed(() => {
+    const maps = new Map<string, CalendarEvent[]>()
+
+    events.forEach((event) => {
+      const date = event.date || Date.toString() // Assuming each event has a 'date' property
+      if (maps.has(date))
+        maps.get(date)?.push(event)
+      else
+        maps.set(date, [event])
+    })
+
+    return maps
+  })
+
+  function getEventsByDate(date: string) {
+    if (!mapEvents.value.has(date))
+      return []
+    return Array.from(mapEvents.value.get(date)?.values() || [])
+  }
+  return {
+    events,
+    mapEvents,
+    getEventsByDate,
+  }
+}
